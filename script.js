e6ef80e4b37f0b0a684af8fc9d5f0ee6a0dc9884
vonhappatsch <vonhappatsch@gmail.com>
2019-02-21 06:55:07 -0300

    Primeiro commit - primeira versão com css básico

---
+++ script.js
@@ -0,0 +1,77 @@
+const button = document.querySelector("#send");
+const tweet = document.querySelector("textarea");
+const tweetBox = document.querySelector("#tweet-box");
+const counter = document.querySelector("#counter");
+const chars = 140;
+
+function tweetList(event) {
+    let content = tweet.value;
+    let list = document.createElement("li");
+    list.setAttribute("class", "styledList");
+
+    let options = {hour: "numeric", minute: "numeric"};
+    let time = new Date().toLocaleDateString("pt-BR", options);
+    time = (time.slice(-5));
+    let listContent = document.createTextNode(time + " - " + content);
+    list.appendChild(listContent);
+
+    let tweets = document.getElementById("tweet-box");
+    tweets.insertBefore(list, tweets.childNodes[0]);
+    
+    clearBox();
+    disableButton();
+    clearCounter(); 
+}
+
+function clearBox() {
+    document.querySelector("textarea").value = "";
+}
+
+function enableButton() {
+    if (tweet !== "") {
+        button.disabled = false;
+        button.setAttribute("style", "color: black;");
+    } 
+}
+
+function disableButton() {
+    button.disabled = true;
+    button.setAttribute("style", "color: grey;");
+}
+
+function counting() {
+    let tracking = chars - tweet.value.length;
+    counter.textContent = tracking;
+
+    if (tracking <= 0) {
+        button.disabled = true;
+        counter.style.color = "grey";
+        button.style.color = "grey";
+    } else if (tracking <= 80 && tracking > 70) {
+        counter.style.color = "orange"
+    } else if (tracking <= 69) {
+        counter.style.color = "red";
+    } else {
+        counter.style.color = "black";
+    }
+}
+
+function cleaning(event) {
+    if (event.keyCode === 8 && document.activeElement === "") {
+        chars = 140;
+    }
+}
+
+function clearCounter() {
+    document.querySelector("#counter").innerHTML = 140;
+}
+
+function boxSize() {
+    tweet.scrollHeight;
+    tweet.css('height', scroll_height + 'px');
+}
+
+button.addEventListener("click", tweetList);
+tweet.addEventListener("keydown", counting);
+tweet.addEventListener("keypress", cleaning);
+tweet.addEventListener("oninput", boxSize);
